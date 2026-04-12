a513303f67404cb77e6579d2b53c5e3ff531078e
Shivam <shivamm.2606@gmail.com>
2026-04-12 23:01:46 +0530

    add user model

---
+++ server/src/models/user.model.ts
@@ -0,0 +1,82 @@
+import mongoose, { Document, Model, Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+interface IUser {
+  name: string;
+  email: string;
+  password: string;
+  refreshToken?: string | null;
+}
+
+interface IUserMethods {
+  isPasswordCorrect(enteredPassword: string): Promise<boolean>;
+  generateAccessToken(): string;
+  generateRefreshToken(): string;
+}
+
+type UserModel = Model<IUser, {}, IUserMethods>;
+
+const userSchema = new Schema<IUser, UserModel, IUserMethods>(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    refreshToken: {
+      type: String,
+      default: null,
+    },
+  },
+  { timestamps: true },
+);
+
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+userSchema.methods.isPasswordCorrect = function (
+  entered_password: string,
+): Promise<boolean> {
+  return bcrypt.compare(entered_password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function (): string {
+  return jwt.sign(
+    { _id: this._id },
+    process.env.ACCESS_TOKEN_SECRET as string,
+    {
+      expiresIn: (process.env.ACCESS_TOKEN_EXPIRY ||
+        "1d") as jwt.SignOptions["expiresIn"],
+    },
+  );
+};
+
+userSchema.methods.generateRefreshToken = function (): string {
+  return jwt.sign(
+    { _id: this._id },
+    process.env.REFRESH_TOKEN_SECRET as string,
+    {
+      expiresIn: (process.env.REFRESH_TOKEN_EXPIRY ||
+        "7d") as jwt.SignOptions["expiresIn"],
+    },
+  );
+};
+
+const User = mongoose.model<IUser, UserModel>("User", userSchema);
+
+export default User;
+export type { IUser, IUserMethods };
