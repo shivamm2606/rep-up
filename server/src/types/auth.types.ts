920eea2ba7e9873bbc1555f4605137a863292ca6
Shivam <shivamm.2606@gmail.com>
2026-04-13 15:20:29 +0530

    create auth interfaces

---
+++ server/src/types/auth.types.ts
@@ -0,0 +1,30 @@
+export interface IAuthService {
+  registerUser(dto: RegisterDto): Promise<AuthUser>;
+  loginUser(dto: LoginDto): Promise<LoginResult>;
+  logoutUser(userId: string): Promise<void>;
+}
+
+export interface RegisterDto {
+  name: string;
+  email: string;
+  username: string;
+  password: string;
+}
+
+export interface LoginDto {
+  email: string;
+  password: string;
+}
+
+export interface AuthUser {
+  _id: string;
+  username: string;
+  name: string;
+  email: string;
+}
+
+export interface LoginResult {
+  user: AuthUser;
+  accessToken: string;
+  refreshToken: string;
+}
