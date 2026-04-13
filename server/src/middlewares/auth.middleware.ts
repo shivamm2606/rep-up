a869720a8e91ee38806493ce71dc25841f837548
Shivam <shivamm.2606@gmail.com>
2026-04-13 15:46:44 +0530

    add verifyjwt middleware

---
+++ server/src/middlewares/auth.middleware.ts
@@ -0,0 +1,28 @@
+import jwt from "jsonwebtoken";
+import { ApiError } from "../utils/apiError.js";
+import asyncHandler from "../utils/asyncHandler.js";
+import User from "../models/user.model.js";
+
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+  const token =
+    req.cookies?.accessToken ||
+    req.header("Authorization")?.replace("Bearer ", "");
+
+  if (!token) {
+    throw new ApiError(401, "Unauthorized");
+  }
+
+  const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET!) as {
+    _id: string;
+  };
+
+  const user = await User.findById(decoded._id);
+
+  if (!user) {
+    throw new ApiError(401, "Invalid token");
+  }
+
+  req.user = user;
+  
+  next();
+});
---
+++ server/src/types/express.d.ts
@@ -0,0 +1,10 @@
+import { Types } from "mongoose";
+import { IUser, IUserMethods } from "./user.types.js";
+
+declare global {
+  namespace Express {
+    interface Request {
+      user: IUser & IUserMethods & { _id: Types.ObjectId };
+    }
+  }
+}   
