7f0a6150fefabef214b7839efe98689bfbaef768
Shivam <shivamm.2606@gmail.com>
2026-05-03 16:18:18 +0530

    feat: add Google Fonts, update useCurrentUser to access correct data structure

--- client/src/hooks/user/useCurrentUser.ts
+++ client/src/hooks/user/useCurrentUser.ts
@@ -4,6 +4,6 @@ import api from "../../lib/axios";
 export const useCurrentUser = () => {
   return useQuery({
     queryKey: ["currentUser"],
-    queryFn: () => api.get("/user/profile").then((r) => r.data),
+    queryFn: () => api.get("/user/profile").then((r) => r.data.data),
   });
 };
