c55b5a396134c5a17ba751310953dfcf49530d80
Shivam <shivamm.2606@gmail.com>
2026-05-03 15:08:42 +0530

    update axios baseURL

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import useAuthStore from "../store/authStore";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: "/api/v1",
   withCredentials: true,
 });
 
